e141a07127c5566a061d7a6585dae5c2f2516b05
Jared Velasquez <jaredvel25@gmail.com>
2024-08-09 22:57:41 -0700

    Modified calculation of tilted cube coordinates and added centering of cube

--- scripts/generate_datasets.py
+++ scripts/generate_datasets.py
@@ -3,6 +3,8 @@ import os
 import sys
 from typing import List, Tuple
 import numpy as np
+from evo.tools import file_interface, plot
+from evo.tools.plot import PlotMode, prepare_axis, traj
 from py_factor_graph.modifiers import (
     split_single_robot_into_multi,
     add_landmark_at_position,
@@ -10,77 +12,82 @@ from py_factor_graph.modifiers import (
 )
 from py_factor_graph.io.g2o_file import parse_3d_g2o_file
 from py_factor_graph.io.pyfg_file import save_to_pyfg_file
+from py_factor_graph.io.tum_file import save_robot_trajectories_to_tum_file
 from py_factor_graph.utils.logging_utils import logger
 from itertools import product
 
 from py_factor_graph.factor_graph import FactorGraphData
 
-# How to use split_single_robot_into_multi_and_add_ranges?
-# split_single_robot_into_multi: 
-# add_inter_robot_range_measurements:
-
-# split_single_robot_into_multi_and_add_ranges adds robot-robot range measurements,
-# not robot-landmark range measurements.
-
-# Could use add_landmark_at_position?
-
 ROBOT_CASES = [2, 5, 10]
 NOISE_STDDEV = [0.10, 0.05]
-CUBE_RADIUS = 10.0
-TILTED_CUBE_RADIUS = 5.0
 
-# Landmark cases are hardcoded; the first 8 landmarks are arranged as a cube that contains 
+# Landmark cases are hard-coded; the first 8 landmarks are arranged as a cube that contains 
 # the sphere encoded by the g2o file; the next 8 landmarks are arranged as a cube that
 # resides inside the first cube, but still contains the sphere.
 
-SENSING_HORIZON = 25.0
+# sphere2500.g2o; observation of xyz data shows that the sphere is centered at 
+# (x = 0.0681175550956	y=-1.0389495768684 z=-50.0233964188792) and radius of ~50m.
+
+CUBE_RADIUS = 70.0
+TILTED_CUBE_RADIUS = 60.0
+CUBE_CENTER = (0.0681175550956, -1.0389495768684, -50.0233964188792)
+
+SENSING_HORIZON = 40.0
 MEAS_PROB = 1.0
 
 def generate_datasets(args) -> None:
     data_fp = args.dataset
     output_dir = args.output_dir
 
-    def generate_cube_coords(radius: float) -> List[Tuple[float, float, float]]:
+    def generate_cube_coords(radius: float, center: Tuple[float, float, float]) -> List[Tuple[float, float, float]]:
         """Generate eight coordinates for a cube.
 
         Args:
             radius (float): radius of the cube
+            center (Tuple[float, float, float]): center of the cube
         
         Returns:
             List[Tuple[float, float, float]]: list of coordinates
         """
+        x, y, z = center
         return [
-            (radius, radius, radius),
-            (radius, radius, -radius),
-            (radius, -radius, radius),
-            (radius, -radius, -radius),
-            (-radius, radius, radius),
-            (-radius, radius, -radius),
-            (-radius, -radius, radius),
-            (-radius, -radius, -radius),
+            (x + radius, y + radius, z + radius),
+            (x + radius, y + radius, z - radius),
+            (x + radius, y - radius, z + radius),
+            (x + radius, y - radius, z - radius),
+            (x - radius, y + radius, z + radius),
+            (x - radius, y + radius, z - radius),
+            (x - radius, y - radius, z + radius),
+            (x - radius, y - radius, z - radius),
         ]
 
-    def generate_tilted_cube_coords(radius: float) -> List[Tuple[float, float, float]]:
+    def generate_tilted_cube_coords(radius: float, center: Tuple[float, float, float]) -> List[Tuple[float, float, float]]:
         """Generate eight coordinates for a cube that is tilted 45 degrees on the z-axis.
 
         Args:
             radius (float): radius of the cube
+            center (Tuple[float, float, float]): center of the cube
         
         Returns:
             List[Tuple[float, float, float]]: list of coordinates
         """
-        return [
-            (radius, 0, radius),
-            (radius, 0, -radius),
-            (0, radius, radius),
-            (0, radius, -radius),
-            (-radius, 0, radius),
-            (-radius, 0, -radius),
-            (0, -radius, radius),
-            (0, -radius, -radius),
-        ]
+
+        # Hard-coded to rotation of 45 degres on the z-axis
+        rot_z = np.matrix([[0.7071, -0.7071, 0.0],
+                           [0.7071, 0.7071, 0.0],
+                           [0.0, 0.0, 1.0]])
+
+        cube_coords = generate_cube_coords(radius, center)
+        tilted_cube_coords = []
+
+        # Convert each coord tuple into a np.ndarray and perform matrix rotation with rot_z
+        for cube_coord in cube_coords:
+            coord = np.asarray(cube_coord)
+            tilted_cube_coords.append(tuple(np.squeeze(np.asarray(rot_z @ coord))))
+        return tilted_cube_coords
+
     
-    def add_landmarks_to_fg(fg, coords: List[Tuple[float, float, float]], range_model: RangeMeasurementModel) -> FactorGraphData:
+    def add_landmarks_to_fg(fg: FactorGraphData, coords: List[Tuple[float, float, float]], range_model: RangeMeasurementModel) -> FactorGraphData:
         new_fg = fg
         for coord in coords:
             new_fg = add_landmark_at_position(new_fg, np.array(coord), range_model)
@@ -94,31 +101,41 @@ def generate_datasets(args) -> None:
         os.makedirs(output_dir)
 
     fg = parse_3d_g2o_file(data_fp)
+    # save_robot_trajectories_to_tum_file(fg, f"{output_dir}", use_ground_truth=False)
 
     prod = product(ROBOT_CASES, NOISE_STDDEV)
-    cube_coords = generate_cube_coords(CUBE_RADIUS)
-    tilted_cube_coords = generate_tilted_cube_coords(TILTED_CUBE_RADIUS)
-    for num_robots, noise in prod:
+    cube_coords = generate_cube_coords(CUBE_RADIUS, CUBE_CENTER)
+    tilted_cube_coords = generate_tilted_cube_coords(TILTED_CUBE_RADIUS, CUBE_CENTER)
+    for i, tup in enumerate(prod):
+        num_robots, noise = tup
         logger.info(f"Creating synthetic datasets for {num_robots} robots with noise stddev {noise}m")
         range_model = RangeMeasurementModel(SENSING_HORIZON, noise, MEAS_PROB)
 
+        case_dir = os.path.join(output_dir, f"{num_robots}_robots_{noise}_stddev")
+
+        if (not os.path.isdir(case_dir)):
+            os.makedirs(case_dir)
+
         fg_mod = split_single_robot_into_multi(fg, num_robots)
         fg_mod = add_landmarks_to_fg(fg_mod, cube_coords, range_model)
-        save_to_pyfg_file(fg_mod, f"{output_dir}/{num_robots}_robots_{noise}_stddev_{len(cube_coords)}_landmarks.pyfg")
+        save_to_pyfg_file(fg_mod, f"{case_dir}/{num_robots}_robots_{noise}_stddev_{len(cube_coords)}_landmarks.pyfg")
         fg_mod = add_landmarks_to_fg(fg_mod, tilted_cube_coords, range_model)
-        save_to_pyfg_file(fg_mod, f"{output_dir}/{num_robots}_robots_{noise}_stddev_{len(cube_coords) + len(tilted_cube_coords)}_landmarks.pyfg")
+        save_to_pyfg_file(fg_mod, f"{case_dir}/{num_robots}_robots_{noise}_stddev_{len(cube_coords) + len(tilted_cube_coords)}_landmarks.pyfg")
+        save_robot_trajectories_to_tum_file(fg_mod, f"{case_dir}", use_ground_truth=False) # For visualization; saves case with 16 range measurements to TUM
 
 def main(args):
     parser = argparse.ArgumentParser(
         description="This script is used to generate multi-robot synthetic datasets with range measurements from an existing g2o file."
     )
     parser.add_argument(
+        "-d",
         "--dataset",
         type=str,
         required=True,
         help="g2o filepath to create synthetic datasets from"
     )
     parser.add_argument(
+        "-o",
         "--output_dir",
         type=str,
         required=True,
